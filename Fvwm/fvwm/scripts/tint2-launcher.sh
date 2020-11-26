ce1cc62d35d07a126bb1c630648ed9dd15d1e8e8
Diego Cesare <cesarediego18@gmail.com>
2020-11-26 20:35:44 -0300

    Foi removido o script para lançar o tema do tint2

--- Fvwm/fvwm/scripts/tint2-launcher.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-Tint=$(cat .gtkrc-2.0 | grep gtk-theme-name | sed 's/.*="//g;s/"//g')
-
-if [[ $Tint = Midnight ]]; then
-	tint2 -c ~/.config/tint2/Dark.tint2rc &
-elif [[ $Tint = Midday ]]; then
-	tint2 -c ~/.config/tint2/Light.tint2rc &
-fi
